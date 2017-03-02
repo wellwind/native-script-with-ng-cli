270ce558bb9ba46fd6d9161b1e8677a85c55adf3
wellwind <wellwind@mail2000.com.tw>
2017-03-02 09:23:58 +0800

    Create NativeScript project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "ns-app",
+    templateUrl: "app.component.html",
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { }
